6c605e9af0e610e13aa7e71c396ca4662eeacfd5
sapmiz25 <sapirmizrahi1402@gmail.com>
2025-11-03 23:22:44 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -6,13 +6,14 @@ let flag;
 for (let i = 2; i <= num; i++) {
   flag = true;
 
+  // Check divisibility from 2 up to i-1
   for (let j = 2; j < i && flag; j++) {
     if (i % j === 0) {
-      flag = false;
+      flag = false; // Found a divisor → not prime
     }
   }
 
   if (flag) {
-    console.log(i);
+    console.log(i); // Print the number if it's prime
   }
 }
