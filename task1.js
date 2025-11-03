e876438b634eb3b8f688655a71e913b8775f7a1f
sapmiz25 <sapirmizrahi1402@gmail.com>
2025-11-03 23:19:11 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -6,16 +6,20 @@ let div2 = Number(num) % 2 === 0;
 let div3 = Number(num) % 3 === 0;
 let div5 = Number(num) % 5 === 0;
 
+// divisible by exactly 1 number
 let num1 =
   (div2 && !div3 && !div5) ||
   (!div2 && div3 && !div5) ||
   (!div2 && !div3 && div5);
 
+// divisible by exactly 2 numbers
 let num2 =
   (div2 && div3 && !div5) || (div2 && !div3 && div5) || (!div2 && div3 && div5);
 
+// divisible by all 3 numbers
 let num3 = div2 && div3 && div5;
 
+// Print how many numbers divide the given number
 console.log(
   "The number: " +
     num +
